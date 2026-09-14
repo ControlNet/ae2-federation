75264c202cd09fd4bbb03ecf45a673d4abccafd2
ControlNet <smczx@hotmail.com>
2026-09-15 08:23:19 +1000

    Add Task 16 provider QA wiring

--- common/src/testmod/java/space/controlnet/ae2federation/test/FederationTestMod.java
+++ common/src/testmod/java/space/controlnet/ae2federation/test/FederationTestMod.java
@@ -24,9 +24,10 @@ public final class FederationTestMod {
         var testId = System.getProperty("ae2federation.testId", "harnessnativesmoke");
         Arrays.stream(new Class<?>[] { FederationGameTests.class, IdentityBaselineGameTests.class, IdentityGameTests.class,
 				 PortGameTests.class, ProviderLaneGameTests.class, EndpointGameTests.class, StorageProofGameTests.class,
-				 NativeCraftingGameTests.class, NativeCraftingFailureGameTests.class, NativeEnergyGameTests.class,
-				   MultipartBridgeGameTests.class, HubGameTests.class, FabricGameTests.class,
-				   FabricBridgeGameTests.class, PolicyLifecycleGameTests.class, PolicyRevisionGameTests.class })
+					 NativeCraftingGameTests.class, NativeCraftingFailureGameTests.class, NativeEnergyGameTests.class,
+					   MultipartBridgeGameTests.class, HubGameTests.class, FabricGameTests.class,
+					   FabricBridgeGameTests.class, PolicyLifecycleGameTests.class, PolicyRevisionGameTests.class,
+					   ProviderLifecycleGameTests.class })
                 .flatMap(testClass -> Arrays.stream(testClass.getDeclaredMethods()))
                 .filter(method -> selected(selection, testId, method))
                 .forEach(event::register);
