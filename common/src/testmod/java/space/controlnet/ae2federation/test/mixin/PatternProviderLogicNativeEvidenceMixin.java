72ca0d76e43011cfba10875f205dd7c9ff7441f2
ControlNet <smczx@hotmail.com>
2026-09-15 22:31:52 +1000

    Add native Endpoint observation Mixins

---
+++ common/src/testmod/java/space/controlnet/ae2federation/test/mixin/PatternProviderLogicNativeEvidenceMixin.java
@@ -0,0 +1,33 @@
+package space.controlnet.ae2federation.test.mixin;
+
+import appeng.api.crafting.IPatternDetails;
+import appeng.api.stacks.KeyCounter;
+import appeng.helpers.patternprovider.PatternProviderLogic;
+import appeng.helpers.patternprovider.PatternProviderTarget;
+import net.minecraft.core.Direction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import space.controlnet.ae2federation.test.processing.NativeEndpointObservation;
+import space.controlnet.ae2federation.test.processing.ProviderRuntimeReplayControl;
+import space.controlnet.ae2federation.test.processing.ProviderTargetObservation;
+
+@Mixin(value = PatternProviderLogic.class, priority = 500)
+public abstract class PatternProviderLogicNativeEvidenceMixin {
+    @Inject(method = "pushPattern(Lappeng/api/crafting/IPatternDetails;[Lappeng/api/stacks/KeyCounter;)Z",
+            at = @At("HEAD"), require = 1)
+    private void ae2federation_test$observePush(IPatternDetails patternDetails, KeyCounter[] inputHolder,
+            CallbackInfoReturnable<Boolean> callback) {
+        NativeEndpointObservation.recordPush(this);
+    }
+
+    @Inject(method = "findAdapter(Lnet/minecraft/core/Direction;)Lappeng/helpers/patternprovider/PatternProviderTarget;",
+            at = @At("HEAD"), require = 1)
+    private void ae2federation_test$observeBoundLookup(Direction side,
+            CallbackInfoReturnable<PatternProviderTarget> callback) {
+        var logic = (PatternProviderLogic) (Object) this;
+        ProviderTargetObservation.recordMixinLookup();
+        ProviderRuntimeReplayControl.observeMixinOwner(logic);
+    }
+}
