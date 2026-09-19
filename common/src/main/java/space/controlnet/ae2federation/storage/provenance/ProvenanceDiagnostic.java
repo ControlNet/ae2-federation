f2cf5a524e024c86bed9bb87e55f087541513bce
ControlNet <smczx@hotmail.com>
2026-09-20 01:22:17 +1000

    Model Task 21 storage generations

---
+++ common/src/main/java/space/controlnet/ae2federation/storage/provenance/ProvenanceDiagnostic.java
@@ -0,0 +1,9 @@
+package space.controlnet.ae2federation.storage.provenance;
+
+public enum ProvenanceDiagnostic {
+    UNSETTLED_ORIGIN,
+    CALLBACK_CHANGED,
+    COMPLETE_AGGREGATE,
+    OPAQUE_EXTERNAL_ALIAS,
+    UNPROVEN_GRID_REBOUND
+}
