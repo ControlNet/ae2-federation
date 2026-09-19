2955e06a54b5b8931ff3a2169d94b08ff8950601
ControlNet <smczx@hotmail.com>
2026-09-20 01:25:29 +1000

    Wire Task 21 storage verification

--- common/src/testmod/java/space/controlnet/ae2federation/test/FederationTestMod.java
+++ common/src/testmod/java/space/controlnet/ae2federation/test/FederationTestMod.java
@@ -25,7 +25,7 @@ public final class FederationTestMod {
         Arrays.stream(new Class<?>[] { FederationGameTests.class, IdentityBaselineGameTests.class, IdentityGameTests.class,
 					 PortGameTests.class, ProviderLaneGameTests.class, EndpointGameTests.class, EndpointModeGameTests.class,
 					 EndpointReturnGameTests.class, EndpointAuthorizationGameTests.class,
-					 StorageProofGameTests.class,
+					 StorageProofGameTests.class, StorageNativeCharacterizationGameTests.class, StorageMountGameTests.class,
 					 NativeCraftingGameTests.class, NativeCraftingFailureGameTests.class, NativeEnergyGameTests.class,
 					   MultipartBridgeGameTests.class, HubGameTests.class, FabricGameTests.class,
 					   FabricBridgeGameTests.class, PolicyLifecycleGameTests.class, PolicyRevisionGameTests.class,
